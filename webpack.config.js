8e7599e91772a47550e2dc923dc8b6322aa66bcb
Creighton Soukup <creightonsoukup@Creightons-MacBook-Pro.local>
2017-11-14 19:44:36 -0500

    react dev environment set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require ('path');
 
-module.exports {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
@@ -11,5 +18,6 @@ module.exports {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
