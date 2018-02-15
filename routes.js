e782090496c08f8b442c184cdf74840503c6ee35
Creighton Soukup <creightonsoukup@Creightons-MacBook-Pro.local>
2018-02-15 11:57:06 -0700

    fix route

--- routes.js
+++ routes.js
@@ -3,13 +3,8 @@ const router = express.Router();
 const db = require('./database/queries')
 
 router.post('/signups', async (req, res, next) => {
-  console.log('hi')
   console.log(req)
-
   res.send('success')
 });
 
 module.exports = router
-
-
-const signups = await db('signup')
