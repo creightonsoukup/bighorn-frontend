f3f1821e3c50448bf0430f8fbfae549f22493a9d
creightonsoukup <creighton@creightonsoukup.com>
2018-05-21 13:56:18 -0400

    updated content

--- client/content/content.js
+++ client/content/content.js
@@ -4,20 +4,20 @@ const content = {
   homepage : {
     textBlock1 : {
       title : "Bighorn Crossing - Redefining Mountain Living",
-      paragraph1: "Bighorn Crossing is refining the idea of mountain living.  There is no need to compromise on luxury indoors in order to feel at one with the outdoors.  We created Bighorn Crossing with this very principle in mind. Bighorn Crossing is a community of 64 townhomes, 72 apartments, and a full service Wyndham Microtel. With access to Arapahoe and Roosevelt National Forest, the Georgetown Lake, local trails and byways, and eight world class ski areas - nature’s finest is at your fingertips",
+      paragraph1: "Bighorn Crossing is refining the idea of mountain living.  There is no need to compromise on luxury indoors in order to feel at one with the outdoors.  We created Bighorn Crossing with this very principle in mind. Bighorn Crossing is a community of 64 townhomes, 72 apartments, a full service Wyndham Microtel and Cabin Creek Brewery. With access to Arapahoe and Roosevelt National Forest, the Georgetown Lake, local trails and byways, and eight world class ski areas - nature’s finest is at your fingertips",
       paragraph2: "Whether interested in a full time residence, a vacation home or a weekend getaway,  there are a variety of townhome layouts, apartment layouts and even a full service hotel to fit your every need.  Of course, you’re thinking where is this mountain oasis? Bighorn Crossing is steps away from the charming, historic town of Georgetown, Colorado and just 40 miles outside of Denver"
     },
     textBlock2 : {
       title: "Charming Mountain Living Meets Contemporary Functionality",
-      paragraph1: "The Bighorn Crossing Townhome is the quintessential mountain home.  Units run at 2 or 3 bedrooms/ 2 and a half baths with a personal outdoor courtyard space and a 1 - 2 car garage. Each unit also includes mountain/lake views, an option to rent through Wyndham, and a completely customizable interior."
+      paragraph1: "The Bighorn Crossing Townhome is the quintessential mountain home.  Units run at 2 or 3 bedrooms/ 2 and a half baths with a personal outdoor courtyard space and a 1 - 2 car garage. Each unit also includes mountain or lake views and an option to rent through our rental program"
     },
     textBlock3: {
       title: "Bighorn Crossing Apartments - Redefining Both the Everyday and the Getaway",
-      paragraph1: "Bighorn Crossing Apartments are ideal for both the every day or a getaway. Apartments list at either 1 or 2 bedrooms/2 full baths, Each apartment offers mountain/lake views and a completely customizable interior.  Your morning coffee has never been more scenic - each unit includes an attached porch space and shared courtyard space."
+      paragraph1: "Bighorn Crossing Apartments are ideal for both the every day or a getaway. Apartments list at either 1 or 2 bedrooms with 1 or 2 full baths, respectively. Each apartment offers mountain or lake views and a clean, modern interior.  Your morning coffee has never been more scenic - each unit includes an attached balcony and outdoor community space."
     },
     textBlock4: {
       title: "Bighorn Crossing Microtel by Wyndham - Redefining R and R",
-      paragraph1: " In need of some R and R?  The Bighorn Crossing community also offers a full service Wyndham Microtel.   The hotel offers 77 rooms designed with the same contemporary touch as the Townhomes and Apartments.  The Bighorn Crossing Wyndham is a destination for those seeking an outdoor retreat with access to fishing, camping, hiking, skiing, etc."
+      paragraph1: " In need of some R and R?  The Bighorn Crossing community also offers a full service Wyndham Microtel.   The hotel offers 77 rooms designed with the same contemporary touch as the Townhomes and Apartments.  The Bighorn Crossing Wynhodham is a destination for those seeking an outdoor retreat with access to fishing, camping, hiking, skiing, etc."
     },
     textBlock5: {
       title: "Georgetown: Quintessential Mountain Town",
@@ -30,25 +30,25 @@ const content = {
   },
   townhomes:  {
     textBlock1: {
-      paragraph1: "The Bighorn Crossing Townhome is modern, spacious, flooded with natural light, pet friendly and ideal for small families.   Cozy up to the gas fireplace included in unit while basking in sprawling mountain and lake views. Make your house a home with customizable interior options through a collaboration with 359 Designs.  Select countertops, appliance finishes, etc to suit you and your family.  Unwind and enjoy access to Wyndham Garden Microtel amenities.",
+      paragraph1: "The Bighorn Crossing Townhome is modern, spacious, flooded with natural light, pet friendly and ideal for small families.   Cozy up to the gas fireplace included in unit while basking in sprawling mountain and lake views. In collaboration with 359 Design, we designed each interior to be clean, modern, and functional. Unwind and enjoy access to Microtel by Wyndham amenities.",
       paragraph2: "Not to mention, each townhomes is just 45 minutes from eight world class ski locations and  steps away from hiking, biking, and fishing, kayaking, and swimming in the Georgetown Lake.  Whether interested in a full time residency or as a second home, the Bighorn Crossing townhome is the quintessential mountain home."
     },
     textBlock2: {
-      paragraph1: "The Bighorn Crossing three bedroom layout includes laundry in unit, gas fireplace, three bedrooms, two and a half bathrooms, and the option for an detached two car garage.  The detached two car garage creates an outdoor courtyard space ideal for entertaining and enjoying the mountains and lake right outside your door."
+      paragraph1: "The Bighorn Crossing three bedroom layout includes laundry in unit, gas fireplace, three bedrooms, two and a half bathrooms, and a detached two car garage.  The detached two car garage creates an outdoor courtyard space ideal for entertaining and enjoying the mountains and lake right outside your door."
     },
     textBlock3: {
-      paragraph1: "The Bighorn Crossing two bedroom layout includes laundry in unit, attached porch space, two bedrooms, two and a half bathrooms and an attached one car garage.  The porch space is ideal for soaking in your sprawling surroundings."
+      paragraph1: "The Bighorn Crossing two bedroom layout includes laundry in unit, two bedrooms, two and a half bathrooms and an attached one car garage.  Although there is no courtyard, there is plenty of community space ideal for soaking in your sprawling surroundings."
     },
     textBlock4: {
       paragraph1: 'The Townhomes were constructed with the mountains in mind.  There are ten different exterior facades all compatible within the historic landscape of Georgetown yet contemporary in design. The exterior was designed in an effort to capture simplicity with muted earth tones and long building facades.  Each townhome was designed at varying height scale blending into the rolling mountain landscape.  Surrounding each townhome are native trees, grasses, and plants and immediate access to a walkable park.'
     },
     textBlock5: {
-      paragraph1: 'Each townhome is the epitome of contemporary luxury. Framed in plated glass bay windows and sprawling wood floors, each unit showcases sleek simplicity.  From white quartz countertops to the stainless steel refrigerator and appliances, the unit’s kitchen is modern and elegant. Every kitchen also includes a stainless steel undermount sink, dishwasher and a garbage disposal.   There are powder rooms in both units with sleek porcelain tile and stainless steel faucets.  Each unit is equipped with individually zoned HVAC systems in the bedrooms and living areas, LED lighting, ample storage space throughout the home and garage, and a washer and dryer included on the bedroom level. Every modern amenity is not only within reach, it is in your home.'
+      paragraph1: 'Each townhome is the epitome of contemporary luxury. Framed in plated glass bay windows and sprawling wood floors, each unit showcases sleek simplicity.  From white quartz countertops to the stainless steel refrigerator and appliances, the unit’s kitchen is modern and elegant. Every kitchen also includes a stainless steel undermount sink, dishwasher and a garbage disposal.   There are powder rooms in both units with sleek porcelain tile and stainless steel faucets.  Each unit is equipped with individually zoned heating systems in the bedrooms and living areas, LED lighting, ample storage space throughout the home and garage, and a washer and dryer included on the bedroom level. Every modern amenity is not only within reach, it is in your home.'
     }
   },
   apartments: {
     textBlock1: {
-      paragraph1: "The Bighorn Crossing Apartment is a spacious, upscale, oasis ideal for couples or small families.  Each apartment is framed in glass plated windows, with the option of 1 or 2 bedrooms, 2 full baths, a kitchen, dining and living area, and an attached porch space.  Every unit offers scenic mountain/lake views and access to all Wyndham Hotel amenities. Ideal for year round residents or vacation stays, the Bighorn Aparment is your slice of paradise in the Colorado wilderness. Learn more about all the apartments have to offer"
+      paragraph1: "The Bighorn Crossing Apartment is a spacious, upscale, oasis ideal for couples or small families.  Each apartment is framed in glass plated windows, with the option of 1 or 2 bedrooms, 2 full baths, a kitchen, dining and living area, and an attached porch space.  Every unit offers scenic mountain/lake views and access to all Wyndham Hotel amenities. Ideal for year round residents or vacation stays, the Bighorn Apartment is your slice of paradise in the Colorado wilderness. Learn more about all the apartments have to offer"
     },
     textBlock2:  {
       paragraph1: "The Apartments effortlessly integrate into the sprawling Georgetown scenery.  There are three major structures, each compatible with the history of the surrounding area yet offer a contemporary update. The exterior was designed in an effort to capture simplicity with muted earth tones and long building facades. Surrounding each townhome are native trees, grasses, and plants and immediate access to a walkable park."
@@ -69,11 +69,11 @@ const content = {
         'Loveland Ski Resort - 13 Miles',
         'Winter Park - 33 Miles',
         'Arapahoe Basin - 20 Miles',
-        'Keystone 31 Miles',
-        'Breckenride 38 Miles',
-        'Copper Mountain 34 Miles',
-        'Vail 53 Miles',
-        'Beaver Creek 65 Miles'
+        'Keystone - 31 Miles',
+        'Breckenridge - 38 Miles',
+        'Copper Mountain - 34 Miles',
+        'Vail 53 - Miles',
+        'Beaver Creek - 65 Miles'
       ]
     },
     recreation: {
@@ -81,7 +81,7 @@ const content = {
       paragraph1: 'Georgetown offers a variety of activities to enjoy nature in all of its glory.  From skiing to lounging by the lake, there is something for everyone and of course all of it with a mountain view - we are in Colorado after all!',
       listItems: [
         "Lake Fishing - Keep an eye out for the Georgetown Bighorn Sheep Herd! You may also come across Trout, Browns, Rainbows and Brookes fish. ",
-        "Lake Activites - Canoeing, kayaking, paddle boarding right outside your door",
+        "Lake Activities - Canoeing, kayaking, paddleboarding right outside your door",
         "White Water Rafting - Enjoy exhilarating rafting and look out for muskrat, beaver, deer, bighorn sheep, and even an occasional Eagle.",
         "Ice Fishing - Bundle up for ice fishing on the frozen lake available seasonally.",
         "Fly Fishing - Visit Clear Creek, a trout river that flows right through Georgetown. Look out for Rainbow and Brown Trout and abundant bug life!",
